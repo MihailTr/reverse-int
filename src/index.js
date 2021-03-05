abe1bedfe68be93c86c4ab9450c82afa95d68ed7
MihailTr <tmyfly@gmail.com>
2021-03-05 13:28:06 +0200

    feat: add reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    return parseInt((''+n).split('').reverse().join(''));
 }
+//console.log(reverse (162));
+
+//
+// let r = ('' + n).split('');
+// let rArr = [];
+// for (let i = r.length; i>=0; i--){
+//     rArr.push(r[i]);
+// }
+// return parseInt(rArr.join(''));
